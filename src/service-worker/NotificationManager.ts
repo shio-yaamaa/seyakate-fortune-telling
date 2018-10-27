6bfbad6043a1e135d686b07faa7bd55e2494147a
shio-yaamaa <shio.yama520@gmail.com>
2018-10-28 02:38:01 +0900

    Use waitUntil() to avoid extra notifications

--- src/service-worker/NotificationManager.ts
+++ src/service-worker/NotificationManager.ts
@@ -3,8 +3,8 @@ import Result from '../utility/Result';
 class NotificationManager {
   private tag = 'seyakate-fortune-telling';
 
-  showNotification(registration: ServiceWorkerRegistration, result: Result, isSilent: boolean) {
-    registration.showNotification(result.toString(), {
+  public async showNotification(registration: ServiceWorkerRegistration, result: Result, isSilent: boolean): Promise<void> {
+    return registration.showNotification(result.toString(), {
       tag: this.tag,
       icon: '/logo.png',
       badge: '/notification-badge.png',
@@ -12,7 +12,7 @@ class NotificationManager {
     });
   }
   
-  handleClick(event: NotificationEvent, clients: Clients) {
+  public handleClick(event: NotificationEvent, clients: Clients) {
     clients.openWindow(process.env.REACT_APP_DEPLOY_URL!);
     event.notification.close();
   }
--- src/service-worker/notification-sw.ts
+++ src/service-worker/notification-sw.ts
@@ -8,27 +8,32 @@ import fetchResult from './fetchResult'; // Different from '../utility/fetchResu
 
 declare var self: ServiceWorkerGlobalScope;
 
-self.addEventListener('push', async () => {
-  try {
-    const resultInDB = await LocalDatabase.getResult(JSTDate.today());
-    let newResult = null;
-    if (resultInDB === null) {
-      const name = await LocalDatabase.getName();
-      if (name === null) throw Error('Could not fetch name from DB');
-      newResult = await fetchResult(name === '' ? DEFAULT_NAME : name);
-      LocalDatabase.setTodaysResult(newResult);
-    }
-
-    if (!(resultInDB === null && newResult === null)) {
-      NotificationManager.showNotification(
+self.addEventListener('push', event => {
+  const reactToPush = async () => {
+    try {
+      const resultInDB = await LocalDatabase.getResult(JSTDate.today());
+      let newResult = null;
+      if (resultInDB === null) {
+        const name = await LocalDatabase.getName();
+        if (name === null) throw Error('Could not fetch name from DB');
+        newResult = await fetchResult(name === '' ? DEFAULT_NAME : name);
+        LocalDatabase.setTodaysResult(newResult);
+      }
+  
+      if (resultInDB === null && newResult === null) {
+        throw Error('Result is not in the DB and could not fetched from the remote server');
+      }
+  
+      await NotificationManager.showNotification(
         self.registration,
         resultInDB === null ? newResult! : resultInDB!,
         resultInDB !== null
       );
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error) {
-    console.log(error);
   }
+  event.waitUntil(reactToPush());
 });
 
 self.addEventListener('notificationclick', (event: NotificationEvent) => {
